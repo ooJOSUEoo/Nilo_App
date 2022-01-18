c1783bfa6c557bb2014ca9bc9b7952f31d7ebbdf
Josue <sjosue5082002@gmail.com>
2022-01-17 18:16:41 -0600

    Adaptador Carrito de Compras

--- app/src/main/java/com/example/nilo/cart/ProductCartFragment.kt
+++ app/src/main/java/com/example/nilo/cart/ProductCartFragment.kt
@@ -1,12 +1,21 @@
 package com.example.nilo.cart
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.nilo.R
 import com.example.nilo.databinding.ItemProductCartBinding
 import com.example.nilo.entities.Product
 
 class ProductCartFragment(private val productList: MutableList<Product>, //lista de productos
-                          private val listener: OnCartListener) { //la interfas
+                          private val listener: OnCartListener) : //la interfas
+    RecyclerView.Adapter<ProductCartFragment.ViewHolder>() {
+
+    private lateinit var context: Context
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val binding = ItemProductCartBinding.bind(view)
@@ -18,6 +27,62 @@ class ProductCartFragment(private val productList: MutableList<Product>, //lista
             binding.ibSub.setOnClickListener { //evento al boton menos
                 listener.setQuantity(product)
             }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_product_cart,parent,false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { //poner los datos en el reciclerView
+        val product = productList[position]
+
+        holder.setListener(product)
+
+        val root = holder.binding
+
+        root.tvName.text = product.name
+        root.tvQuantity.text = product.quantity.toString()
+
+        Glide.with(context)
+            .load(product.imgUrl)//de donde
+            .diskCacheStrategy(DiskCacheStrategy.ALL) //con cache
+            .placeholder(R.drawable.ic_access_time) //fondo si no hay imagen
+            .error(R.drawable.ic_broken_image)//si hay un error con la imagen
+            .centerCrop() //ajustarla
+            .circleCrop()
+            .into(root.imgProduct) //a donde
+    }
+
+    override fun getItemCount(): Int = productList.size
+
+    //metodos para que el carrito sea dinamico
+
+    fun add(product: Product){//añadir y visualizar los elementos de forma local
+        if (!productList.contains(product)){//si el listado actual no contiene el producto, lo agrega
+            productList.add(product) //add es añadir
+            notifyItemInserted(productList.size - 1)//se añade al final
+        }else{ //si existe el producto lo actualiza
+            update(product)
+        }
+    }
+
+    fun update(product: Product){//actualiza el producto
+        val index = productList.indexOf(product) // es el id
+        if (index != -1){//se actualiza
+            productList.set(index,product) //set es actualizar
+            notifyItemChanged(index)//se añade al final
+        }
+    }
+
+    fun delete(product: Product){//elimina el producto
+        val index = productList.indexOf(product) // es el id
+        if (index != -1){//se actualiza
+            productList.removeAt(index)//removeAt es eliminar
+            notifyItemRemoved(index)//se añade al final
         }
     }
 }
